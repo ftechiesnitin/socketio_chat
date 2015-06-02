3a0af24f3c5dbb6f432e74daead08af3a24346a3
Nitin Dolli <nitin.dolli@gmail.com>
2015-06-02 15:30:32 +0530

    testing and debugging

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ io.on('connection', function (socket) {
   console.log('User Connected');
   // when the client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
+    console.log(data);
     // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
       username: socket.username,
@@ -32,6 +33,7 @@ io.on('connection', function (socket) {
 
   // when the client emits 'add user', this listens and executes
   socket.on('add user', function (username) {
+    console.log(username);
     // we store the username in the socket session for this client
     socket.username = username;
     // add the client's username to the global list
